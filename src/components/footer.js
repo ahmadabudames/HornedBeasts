156240342d13538c93b0ac3775d0c3533778f38f
ahmadabudames <ahmadabudamesmais1999@gmail.com>
2021-06-23 16:58:00 +0300

    finished the form part

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div>
+            <p>Done by ahmad abudames</p>
+            </div>
+        )
+    }
+} 
+
+export default Footer;
+
